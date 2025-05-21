e44940e262c68c3223dc0db43519fdb8110db2d9
tatsuyoshikobie <tatsuyoshi.kobie@rakus-partners.co.jp>
2025-05-21 09:38:57 +0900

    変更内容をここに書く

--- src/node/index.js
+++ src/node/index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
-const port = 自分の社員番号;
+const port = 4798;
 
 const cors = require("cors");
 app.use(cors());
 
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "localhost",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_4798", // PostgreSQLのユーザー名に置き換えてください
+  host: "postgres",
+  database: "crm_4798", // PostgreSQLのデータベース名に置き換えてください
+  password: "pass_4798", // PostgreSQLのパスワードに置き換えてください
   port: 5432,
 });
 
@@ -37,7 +37,7 @@ app.post("/add-customer", async (req, res) => {
   try {
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
@@ -48,3 +48,6 @@ app.post("/add-customer", async (req, res) => {
 });
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Hello, World!!");
+});
